3bc2128d676ab038c04d75e77b1200b35b07538b
Hans-Eliseo Fernandez <hanseliseofk@gmail.com>
2026-08-07 12:59:29 +0000

    style(mc-manager-server): replace admin menu icon with creeper

--- wordpress/wp-content/plugins/mc-manager-server/src/Core/Dashboard.php
+++ wordpress/wp-content/plugins/mc-manager-server/src/Core/Dashboard.php
@@ -31,7 +31,7 @@ final class Dashboard
             'manage_options',
             'gestor-mc-srv',
             [$this, 'renderDashboard'],
-            'dashicons-shield',
+            'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAxMDI0IDEwMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZmlsbD0iYmxhY2siIGQ9Ik0xNzAuNjY2NjY3IDg1LjMzMzMzM2g2ODIuNjY2NjY2YTg1LjMzMzMzMyA4NS4zMzMzMzMgMCAwIDEgODUuMzMzMzM0IDg1LjMzMzMzNHY2ODIuNjY2NjY2YTg1LjMzMzMzMyA4NS4zMzMzMzMgMCAwIDEtODUuMzMzMzM0IDg1LjMzMzMzNEgxNzAuNjY2NjY3YTg1LjMzMzMzMyA4NS4zMzMzMzMgMCAwIDEtODUuMzMzMzM0LTg1LjMzMzMzNFYxNzAuNjY2NjY3YTg1LjMzMzMzMyA4NS4zMzMzMzMgMCAwIDEgODUuMzMzMzM0LTg1LjMzMzMzNG04NS4zMzMzMzMgMTcwLjY2NjY2N3YxNzAuNjY2NjY3aDE3MC42NjY2Njd2ODUuMzMzMzMzSDM0MS4zMzMzMzN2MjU2aDg1LjMzMzMzNHYtODUuMzMzMzMzaDE3MC42NjY2NjZ2ODUuMzMzMzMzaDg1LjMzMzMzNHYtMjU2aC04NS4zMzMzMzR2LTg1LjMzMzMzM2gxNzAuNjY2NjY3VjI1NmgtMTcwLjY2NjY2N3YxNzAuNjY2NjY3aC0xNzAuNjY2NjY2VjI1NkgyNTZ6Ii8+PC9zdmc+Cg==',
             58
         );
 
